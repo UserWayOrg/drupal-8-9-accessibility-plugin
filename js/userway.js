9cb7b88733ebe707bb3bd3ae949cf9e67bd6b06d
Radion Chernyshov <radik.chernyshov@gmail.com>
2021-11-18 22:02:34 +0200

    inital commit

---
+++ js/userway.js
@@ -0,0 +1,89 @@
+(function ($) {
+
+    $(document).ready(function () {
+
+        const MESSAGE_ACTION_TOGGLE = 'WIDGET_TOGGLE';
+        const MESSAGE_ACTION_SIGNUP = 'WIDGET_SIGNUP';
+        const MESSAGE_ACTION_SIGNIN = 'WIDGET_SIGNIN';
+
+        const request = (url, data) => {
+            return new Promise((resolve, reject) => {
+                jQuery.ajax({
+                    url,
+                    type: 'POST',
+                }).done(data => resolve(data))
+                    .fail(err => reject(err));
+            })
+        }
+
+        const requestUpdateAccountState = (url, accountId, state) => {
+            return request(url + '/admin/userway/account?accountId=' + accountId + '&state=' + state, {
+                accountId,
+                state
+            })
+        };
+
+        const isPostMessageSignAction = (postMessage) => {
+            return postMessage.data !== undefined
+                && postMessage.data.action
+                && postMessage.data.action === MESSAGE_ACTION_SIGNIN
+        }
+
+        const isPostMessageAccountToggleAction = (postMessage) => {
+            return postMessage.data !== undefined
+                && postMessage.data.action
+                && postMessage.data.action === MESSAGE_ACTION_TOGGLE
+        }
+
+        const isPostMessageSignupAction = (postMessage) => {
+            return postMessage.data !== undefined
+                && postMessage.data.action
+                && postMessage.data.action === MESSAGE_ACTION_SIGNUP
+        }
+
+        const selector = document.getElementById('userway-frame');
+        const frameContentWindow = selector.contentWindow;
+        const {url} = selector.dataset;
+
+        window.addEventListener('message', postMessage => {
+            if (!postMessage || postMessage.source !== frameContentWindow) {
+                console.log('postMessage -> missed data');
+                return;
+            }
+
+            console.log('postMessage -> ', postMessage);
+            console.log('postMessage data -> ', postMessage.data);
+            console.log('postMessage isPostMessageSignupAction -> ', isPostMessageSignupAction(postMessage));
+            console.log('postMessage isPostMessageAccountToggleAction -> ', isPostMessageAccountToggleAction(postMessage));
+            console.log('postMessage isPostMessageSignAction -> ', isPostMessageSignAction(postMessage));
+
+            if (isPostMessageAccountToggleAction(postMessage)) {
+                const state = postMessage.data.state ? 1 : 0;
+                const account = postMessage.data?.account;
+
+                if (!account) {
+                    console.error('[userway] account not found');
+                    return
+                }
+
+                requestUpdateAccountState(decodeURIComponent(url), account, state)
+                    .then(res => console.log(res))
+                    .catch(err => console.error(err));
+
+            } else if (isPostMessageSignupAction(postMessage) || isPostMessageSignAction(postMessage)) {
+                const state = postMessage.data.state ? 1 : 0;
+                const account = postMessage.data.account;
+
+                if (!account) {
+                    console.error('[userway] account not found');
+                    return
+                }
+
+                requestUpdateAccountState(decodeURIComponent(url), account, state)
+                    .then(res => console.log(res))
+                    .catch(err => console.error(err));
+            }
+        });
+    });
+})(jQuery);
+
---
+++ src/Controller/ConfigurationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\userway\Controller;
+
+use Drupal;
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class ConfigurationController.
+ */
+class ConfigurationController extends ControllerBase {
+
+  /**
+   * Index
+   */
+  public function index() {
+
+    $widgetState = 'false';
+    $widgetAccount = '';
+    $database = Drupal::database();
+    $query = $database->query('SELECT account, status FROM {userway_data} LIMIT 1');
+    $widgetData = $query->fetchAssoc();
+    $rootUrl = rtrim(\Drupal\Core\Url::fromUserInput('/', ['absolute' => TRUE])
+      ->toString(), '/');
+
+    if (isset($widgetData['account']) && isset($widgetData['status'])) {
+      $widgetState = $widgetData['status'] === '1' ? 'true' : 'false';
+      $widgetAccount = $widgetData['account'] ?: '';
+    }
+
+    $frameUrl = "https://api.userway.org/api/apps/drupal?storeUrl={$rootUrl}";
+
+    if ($widgetAccount !== '') {
+      $frameUrl .= '&account_id=' . $widgetAccount;
+      $frameUrl .= '&active=' . $widgetState;
+    }
+
+    return [
+      '#theme' => 'userway',
+      '#attached' => [
+        'library' => [
+          'userway/userway',
+        ],
+      ],
+      '#url' => $frameUrl,
+      '#rootUrl' => $rootUrl,
+    ];
+  }
+
+}
